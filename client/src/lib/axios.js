5351acf6d77d13f0662928d41b5f27062aee143d
Gourav Manjhi <gouravmanjhi9313@gmail.com>
2026-02-15 18:09:56 +0530

    frontend production ready

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
 });
-
 
 instance.interceptors.request.use(async (config) => {
   if (window.Clerk) {
